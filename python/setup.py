0ca5cf0e426bd19824d6ee696ec888cd6b29d052
helenaeverleyz <helenaeverleyz@gmail.com>
2025-02-14 10:51:24 -0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pocketflow",
+    name="pocket",
     version="0.0.1",
     packages=find_packages(),
-    author="Zachary Huang",
-    author_email="zh2408@columbia.edu",
-    description="Minimalist LLM Framework in 100 Lines. Enable LLMs to Program Themselves.",
-    url="https://github.com/miniLLMFlow/PocketFlow/",
-)
+    author="Helena Zhang",
+    author_email="helenaeverleyz@gmail.com",
+    description="LLM Framework for LLMs. Enable LLMs to Program Themselves.",
+    url="https://github.com/helenaeverleyz/Pocket/",
+) 
